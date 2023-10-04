9524b6142446ab0d39a50cf0da162925ad451022
Barak Amar <barak.amar@treeverse.io>
2023-10-04 11:16:11 +0300

    keep bi report for restore repository refs

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -3320,7 +3320,7 @@ func (c *Controller) RestoreRefsSubmit(w http.ResponseWriter, r *http.Request, b
 		return
 	}
 	ctx := r.Context()
-	c.LogAction(ctx, "restore_refs_submit", r, repository, "", "")
+	c.LogAction(ctx, "restore_repository_refs", r, repository, "", "")
 
 	info := &catalog.RefsDumpInfo{
 		CommitsMetarangeId:  body.CommitsMetaRangeId,
